113b0e5fb2daea658ee54bef0769b31a3c99aa26
vipinjoshi <vipiswaiting@live.com>
2017-06-13 16:00:24 +0530

    create development branch

--- ng2-fundamentals-master/app/events/event-list.component.ts
+++ ng2-fundamentals-master/app/events/event-list.component.ts
@@ -1,10 +1,10 @@
-import {Component,OnInit} from '@angular/core';
-import {EventsService} from './shared/event.service';
-import {ToastrService} from '../common/toastr.service'
+import { Component, OnInit } from '@angular/core';
+import { EventsService } from './shared/event.service';
+import { ToastrService } from '../common/toastr.service'
 
 @Component({
-    selector:'events-list',
-    template:`<div>   
+    selector: 'events-list',
+    template: `<div>   
       
       <h1>
       
@@ -18,34 +18,33 @@ import {ToastrService} from '../common/toastr.service'
     </div>
     
     </div>`
-    
+
 
 })
 //to call output (myEventClick)="handleClickMe($event)"
 //[event is the property name of thumbnail component and event1 is the input parameter we passing to [event]
 //{{}} is called interpolation and known as one way binding
- //to show that in the app controller use the selection in app module
-    export class EventListComponent  implements OnInit
-    {
-      events:any[]
-  constructor(private eventService:EventsService,private toastrService:ToastrService){
-    //events=this.eventService.getEvents();
-//its recomend to not use ajax call in constructor as it might take time so for that we must use 
-// lifecycle @ngInit hook to get the data for services so the code move to ngInt
-  }
-  ngOnInit(){
-this.events=this.eventService.getEvents()
-  }
-
-  
-handleThumbnailClick(name){
-  /*  toastr.success(name); as its global so we get error in command prompt screen that cannot
-  find the name toastr and also it will make the code untestable so to overcome with that we need 
-  to create our own service and register this toastr there*/
-  this.toastrService.success(name)
-} 
-
+//to show that in the app controller use the selection in app module
+export class EventListComponent implements OnInit {
+    events: any[]
+    constructor(private eventService: EventsService, private toastrService: ToastrService) {
+        //events=this.eventService.getEvents();
+        //its recomend to not use ajax call in constructor as it might take time so for that we must use 
+        // lifecycle @ngInit hook to get the data for services so the code move to ngInt
     }
+    ngOnInit() {
+        this.events = this.eventService.getEvents()
+    }
+
+
+    handleThumbnailClick(name) {
+        /*  toastr.success(name); as its global so we get error in command prompt screen that cannot
+        find the name toastr and also it will make the code untestable so to overcome with that we need 
+        to create our own service and register this toastr there*/
+        this.toastrService.success(name)
+    }
+
+}
 
 
 /*/
