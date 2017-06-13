2603c6c3358af8dad9e9b5861ea28b84272fa657
vipinjoshi <vipiswaiting@live.com>
2017-06-13 16:13:41 +0530

    format docs

--- ng2-fundamentals-master/app/events/event-thumbnail.component.ts
+++ ng2-fundamentals-master/app/events/event-thumbnail.component.ts
@@ -1,25 +1,25 @@
-import {Component,Input,Output,EventEmitter} from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 
 @Component({
-    selector:'events-thumbnail',
-    templateUrl:'app/events/event-list.component.html',
-    styles:[
+    selector: 'events-thumbnail',
+    templateUrl: 'app/events/event-list.component.html',
+    styles: [
         `.pad-left{ margin-left :10px}
         .thumbnail {min-height:220px}
         `
-        
+
     ]
 })
 
-export class EventThumbnailComponent{
- @Input() event:any
+export class EventThumbnailComponent {
+    @Input() event: any
 
-/* used in tempelate variable  logFromParent(){
-     console.log('hello child you are call from parent')
- }
-@Output() myEventClick =new EventEmitter();
-handleClickMe(){
-    this.myEventClick.emit('hello from child') //console.log("I am clicked")
-}*/
+    /* used in tempelate variable  logFromParent(){
+         console.log('hello child you are call from parent')
+     }
+    @Output() myEventClick =new EventEmitter();
+    handleClickMe(){
+        this.myEventClick.emit('hello from child') //console.log("I am clicked")
+    }*/
 }
--- ng2-fundamentals-master/app/nav/navbar.component.ts
+++ ng2-fundamentals-master/app/nav/navbar.component.ts
@@ -1,15 +1,15 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-    selector:'navbar',
-    templateUrl:'app/nav/navbar.component.html',
-    styles:[`
+    selector: 'navbar',
+    templateUrl: 'app/nav/navbar.component.html',
+    styles: [`
         .nav.navbar-nav{font-size:15px; }
         #searchForm {margin-right:100px;}
         @media(max-width:1200px){#searchForm{display:none}}    
     `]
 })
 
-export class NavBarComponent{
+export class NavBarComponent {
 
 }
